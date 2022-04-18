77fffe45523e254ae3dc4ad753165826cdb53ad2
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2022-04-18 23:29:18 +0800

    Initial Commit

---
+++ Quality/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quality
+//
+//  Created by Vincent Neo on 18/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quality/QualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  QualityApp.swift
+//  Quality
+//
+//  Created by Vincent Neo on 18/4/22.
+//
+
+import SwiftUI
+
+@main
+struct QualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
