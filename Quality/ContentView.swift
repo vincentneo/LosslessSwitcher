86c187d16f68f3a1efe86067f74c85209b66f6c3
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2022-04-20 00:56:01 +0800

    split out device updates on to its own observable object

--- Quality/ContentView.swift
+++ Quality/ContentView.swift
@@ -10,39 +10,42 @@ import OSLog
 import SimplyCoreAudio
 
 struct ContentView: View {
+    @StateObject var outputDevices = OutputDevices()
     let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
-    let coreAudio = SimplyCoreAudio()
     
     var body: some View {
         Text("Hello, world!")
             .padding()
-            .onAppear {
-                print(coreAudio.defaultOutputDevice)
-                print(coreAudio.defaultOutputDevice?.nominalSampleRates)
-                //AudioDeviceFinder.findDevices()
-            }
             .onReceive(timer) { input in
                 DispatchQueue.main.async {
-                    do {
-                        let musicLog = try Console.getRecentEntries()
-                        let cmStats = CMPlayerParser.parseMusicConsoleLogs(musicLog)
-                        if let first = cmStats.first, let supported = coreAudio.defaultOutputDevice?.nominalSampleRates {
-                            let sampleRate = Float64(first.sampleRate)
-                            // https://stackoverflow.com/a/65060134
-                            let nearest = supported.enumerated().min(by: {
-                                abs($0.element - sampleRate) < abs($1.element - sampleRate)
-                            })
-                            if let nearest = nearest {
-                                let nearestSampleRate = nearest.element
-                                coreAudio.defaultOutputDevice?.setNominalSampleRate(nearestSampleRate)
-                            }
-                        }
-                    }
-                    catch {
-                        print(error)
+                    self.switchLatestSampleRate()
+                }
+            }
+    }
+    
+    func switchLatestSampleRate() {
+        do {
+            let musicLog = try Console.getRecentEntries()
+            let cmStats = CMPlayerParser.parseMusicConsoleLogs(musicLog)
+            
+            let defaultDevice = outputDevices.defaultOutputDevice
+            if let first = cmStats.first, let supported = defaultDevice?.nominalSampleRates {
+                let sampleRate = Float64(first.sampleRate)
+                // https://stackoverflow.com/a/65060134
+                let nearest = supported.enumerated().min(by: {
+                    abs($0.element - sampleRate) < abs($1.element - sampleRate)
+                })
+                if let nearest = nearest {
+                    let nearestSampleRate = nearest.element
+                    if nearestSampleRate != defaultDevice?.nominalSampleRate {
+                        defaultDevice?.setNominalSampleRate(nearestSampleRate)
                     }
                 }
             }
+        }
+        catch {
+            print(error)
+        }
     }
 }
 
---
+++ Quality/OutputDevices.swift
@@ -0,0 +1,39 @@
+//
+//  OutputDevices.swift
+//  Quality
+//
+//  Created by Vincent Neo on 20/4/22.
+//
+
+import Combine
+import Foundation
+import SimplyCoreAudio
+
+class OutputDevices: ObservableObject {
+    @Published var defaultOutputDevice: AudioDevice?
+    @Published var outputDevices = [AudioDevice]()
+    
+    private let coreAudio = SimplyCoreAudio()
+    
+    private var changesCancellable: AnyCancellable?
+    private var defaultChangesCancellable: AnyCancellable?
+    
+    init() {
+        self.outputDevices = self.coreAudio.allOutputDevices
+        self.defaultOutputDevice = self.coreAudio.defaultOutputDevice
+        changesCancellable =
+            NotificationCenter.default.publisher(for: .deviceListChanged).sink(receiveValue: { _ in
+                self.outputDevices = self.coreAudio.allOutputDevices
+            })
+        
+        defaultChangesCancellable =
+            NotificationCenter.default.publisher(for: .defaultOutputDeviceChanged).sink(receiveValue: { _ in
+                self.defaultOutputDevice = self.coreAudio.defaultOutputDevice
+            })
+    }
+    
+    deinit {
+        changesCancellable?.cancel()
+        defaultChangesCancellable?.cancel()
+    }
+}
