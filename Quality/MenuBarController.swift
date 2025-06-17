bd7facca08957c9483be7627006511878cec629b
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2025-06-18 01:33:16 +0800

    preparations to fix blank window issue

---
+++ Quality/MenuBarController.swift
@@ -0,0 +1,26 @@
+//
+//  MenuBarController.swift
+//  LosslessSwitcher
+//
+//  Created by Vincent Neo on 18/6/25.
+//
+
+import Observation
+import SwiftUI
+
+@Observable
+class MenuBarController {
+    var statusBarTitle = "Loading"
+    
+    @ObservationIgnored
+    var outputDevices: OutputDevices!
+    
+    @ObservationIgnored
+    private var mrController: MediaRemoteController!
+    
+    init() {
+        let outputDevices = OutputDevices()
+        self.outputDevices = outputDevices
+        self.mrController = MediaRemoteController(outputDevices: outputDevices)
+    }
+}
--- Quality/QualityApp.swift
+++ Quality/QualityApp.swift
@@ -12,9 +12,90 @@ struct QualityApp: App {
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    @State private var controller = MenuBarController()
+    @ObservedObject private var defaults = Defaults.shared
+    
     var body: some Scene {
-        Settings {
-            EmptyView()
+        MenuBarExtra {
+            MenuView()
+                .environmentObject(controller.outputDevices)
+                .environmentObject(defaults)
+        } label: {
+            if defaults.userPreferIconStatusBarItem {
+                Image(systemName: "music.note")
+            }
+            else {
+                Text(controller.statusBarTitle)
+            }
+        }
+        .menuBarExtraStyle(.menu)
+    }
+}
+
+struct MenuView: View {
+    
+    @EnvironmentObject private var outputDevices: OutputDevices
+    @EnvironmentObject private var defaults: Defaults
+    
+    var body: some View {
+        VStack {
+            ContentView()
+            
+            Divider()
+            
+            Button {
+                defaults.userPreferIconStatusBarItem.toggle()
+            } label: {
+                Text(defaults.statusBarItemTitle)
+            }
+            
+            Button {
+                defaults.userPreferBitDepthDetection.toggle()
+            } label: {
+                HStack {
+                    Text("Bit Depth Switching")
+                    if defaults.userPreferBitDepthDetection {
+                        Image(systemName: "checkmark")
+                    }
+                }
+            }
+            
+            Menu {
+                Button {
+                    outputDevices.selectedOutputDevice = nil
+                    defaults.selectedDeviceUID = nil
+                } label: {
+                    Text("Default Device")
+                }
+
+                ForEach(outputDevices.outputDevices, id: \.uid) { device in
+                    Button {
+                        outputDevices.selectedOutputDevice = device
+                        defaults.selectedDeviceUID = device.uid
+                    } label: {
+                        Text(device.name)
+                        if outputDevices.selectedOutputDevice?.uid == device.uid {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+            } label: {
+                Text("Selected Device")
+            }
+            
+            Menu {
+                
+            } label: {
+                Text("")
+            }
+            
+            Menu {
+                Text("Version - \(currentVersion)")
+                Text("Build - \(currentBuild)")
+            } label: {
+                Text("About")
+            }
         }
     }
 }
+
