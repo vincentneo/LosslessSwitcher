cb6aff6f1867555d6e8bce5f7381be2786a1f8db
pdevh <devphmail@gmail.com>
2025-11-04 20:21:09 +0100

    Add display bit-depth feature

--- Quality/SampleRateLabel.swift
+++ Quality/SampleRateLabel.swift
@@ -11,10 +11,16 @@ struct SampleRateLabel: View {
     @EnvironmentObject private var outputDevices: OutputDevices
     var body: some View {
         if let currentSampleRate = outputDevices.currentSampleRate {
-            let formattedSampleRate = String(format: "%.1f kHz", currentSampleRate)
-            Text(formattedSampleRate)
-        }
-        else {
+            if outputDevices.enableBitDepthDetection {
+                if let bitDepth = outputDevices.currentBitDepth {
+                    Text(String(format: "%.1f kHz / %d bit", currentSampleRate, bitDepth))
+                } else {
+                    Text(String(format: "%.1f kHz / ? bit", currentSampleRate))
+                }
+            } else {
+                Text(String(format: "%.1f kHz", currentSampleRate))
+            }
+        } else {
             Text("Unknown")
         }
     }
